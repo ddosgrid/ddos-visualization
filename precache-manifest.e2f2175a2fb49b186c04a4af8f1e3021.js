72cd2fa878c3545bab49781dbe4d80385f8892d8
jvdassen <jan.vda@gmx.ch>
2020-01-27 10:02:34 +0000

    Deploying to gh-pages from  - c7732b74a249c9f8ffd0b67ea6c801b05f711fba 🚀

---
+++ precache-manifest.e2f2175a2fb49b186c04a4af8f1e3021.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90de5c81ea5428da70cb",
+    "url": "/css/about.c39653a6.css"
+  },
+  {
+    "revision": "9ecb66ede7b99049ff49",
+    "url": "/css/app.854df249.css"
+  },
+  {
+    "revision": "a6a46b95176614b79921",
+    "url": "/css/chunk-vendors.7caec713.css"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/img/github.svg"
+  },
+  {
+    "revision": "c4743842bcbaf98e949a807d04b17139",
+    "url": "/img/multiline_chart-24px.svg"
+  },
+  {
+    "revision": "70c9588c763088af80c021a055980796",
+    "url": "/img/search-24px.svg"
+  },
+  {
+    "revision": "763d43aff7b9ac9187195d87ec5d4c22",
+    "url": "/img/show_chart-24px.svg"
+  },
+  {
+    "revision": "49ae8db854f71ce37b6ab2c58d9736ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90de5c81ea5428da70cb",
+    "url": "/js/about.f288aeb3.js"
+  },
+  {
+    "revision": "9ecb66ede7b99049ff49",
+    "url": "/js/app.4c364760.js"
+  },
+  {
+    "revision": "a6a46b95176614b79921",
+    "url": "/js/chunk-vendors.2156c548.js"
+  },
+  {
+    "revision": "cc8f7d8bf776c48ed757a07a5787af32",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "763d43aff7b9ac9187195d87ec5d4c22",
+    "url": "/show_chart-24px.svg"
+  }
+]);
