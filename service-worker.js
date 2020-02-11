8614fb6382c8a0bc137c0594ba549445f1140f09
jvdassen <jan.vda@gmx.ch>
2020-02-11 09:54:08 +0000

    Deploying to gh-pages from  - 1e6c0872b123df96806d317252cbb744c0489a26 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.80a141198c1ec2ebc35918185444d457.js"
+  "/precache-manifest.5feced3c87207d11e2bded99278e148a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "ddosgrid.online"});
